754a81f4efbcb3bcd036a7fa2cfdf8a364fbbb66
Daniel Verner <daniel.verner@42coders.com>
2019-06-17 11:55:35 +0200

    Invoice template class

--- app/DocumentTemplates/InvoiceTemplate.php
+++ app/DocumentTemplates/InvoiceTemplate.php
@@ -7,7 +7,7 @@ use App\User;
 use BWF\DocumentTemplates\DocumentTemplates\DocumentTemplate;
 use BWF\DocumentTemplates\DocumentTemplates\DocumentTemplateInterface;
 
-class DemoDocumentTemplate2 implements DocumentTemplateInterface
+class InvoiceTemplate implements DocumentTemplateInterface
 {
     use DocumentTemplate;
     use DemoTemplateData;
@@ -15,7 +15,8 @@ class DemoDocumentTemplate2 implements DocumentTemplateInterface
     protected function dataSources()
     {
         return [
-            $this->dataSource($this->getTestObject(), 'test'),
+            $this->dataSource($this->testOrders[0], 'order', true, 'orders'),
+            $this->dataSource(new User(), 'user'),
         ];
     }
 
