f7fcf1979bb20c38608fcc2edcb2fd24d5bb3957
Daniel Verner <daniel.verner@42coders.com>
2019-09-13 15:41:44 +0200

    Change the invoide template, maked the editable part valid html

--- database/seeds/EditableTemplatesTableSeeder.php
+++ database/seeds/EditableTemplatesTableSeeder.php
@@ -24,6 +24,15 @@ class EditableTemplatesTableSeeder extends Seeder
             'document_template_id' => $documentTemplate->id,
             'name' => 'order_items',
             'content' => '
+    <table width="100%">
+        <thead>
+        <tr>
+            <th>Description</th>
+            <th>Quantity</th>
+            <th>Total</th>
+        </tr>
+        </thead>
+        <tbody>
                 {% for order in orders %}
                     <tr>
                         <td>
@@ -35,6 +44,29 @@ class EditableTemplatesTableSeeder extends Seeder
                         </td>
                     </tr>
                 {% endfor %}
+                            </tbody>
+
+        <tfoot>
+        <tr>
+            <td colspan="1"></td>
+            <td align="left">Total</td>
+            <td align="left" class="gray">€60,-</td>
+        </tr>
+        </tfoot>
+    </table>
+                '
+        ])->save();
+
+        \BWF\DocumentTemplates\EditableTemplates\EditableTemplate::create([
+            'document_template_id' => $documentTemplate->id,
+            'name' => 'customer_details',
+            'content' => '
+                <h3>{{user.name}}</h3>
+                <pre>
+Street 15
+123456 City
+United Kingdom
+</pre>
                 '
         ])->save();
 
