5e9f75a8e62d5de753cf6c9d16208d0cfe37f6b7
Daniel Werner <vernerd@gmail.com>
2024-08-05 16:10:38 +0200

    Add newsletter to seeders

--- database/seeders/EditableTemplatesTableSeeder.php
+++ database/seeders/EditableTemplatesTableSeeder.php
@@ -71,5 +71,27 @@ United Kingdom
 </pre>
                 '
         ])->save();
+
+
+        $documentTemplate = \BWF\DocumentTemplates\DocumentTemplates\DocumentTemplateModel::create([
+            'name' => 'Newsletter',
+            'document_class' => \App\DocumentTemplates\NewsletterTemplate::class,
+            'layout' => 'NewsletterTemplate.html.twig'
+        ]);
+
+        $documentTemplate->save();
+
+
+        \BWF\DocumentTemplates\EditableTemplates\EditableTemplate::create([
+            'document_template_id' => $documentTemplate->id,
+            'name' => 'title',
+            'content' => 'This is the title'
+        ])->save();
+
+        \BWF\DocumentTemplates\EditableTemplates\EditableTemplate::create([
+            'document_template_id' => $documentTemplate->id,
+            'name' => 'content',
+            'content' => '<p>This is the content</p>'
+        ])->save();
     }
 }
